07b405bff05ced10e205186d888fc24c51e2c27e
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2025-04-17 11:42:42 -0700

    Simplified bin trie

--- data_structure/tree/trie/cxx/src/BinTrie.cpp
+++ data_structure/tree/trie/cxx/src/BinTrie.cpp
@@ -5,23 +5,25 @@ using namespace std;
 
 // Please refer to this post https://leetcode.com/problems/dot-product-of-two-sparse-vectors/solutions/6658012/two-bin-trie-lcrs-and-dfs
 
+
+
+
 template <typename T, typename INTKEY>
-struct LL
+struct LL final
 {
-    LL(INTKEY aKey): key(aKey) {}
-    INTKEY key = 0;
+    LL(INTKEY aKey): node(aKey) {}
     T node;
     unique_ptr<LL<T,INTKEY>> next = nullptr;
 };
 
 template<typename INTKEY,typename INTVAL, const int ORDER=2>
-struct BinTrie
+struct BinTrie final
 {
     using BinTreeLink = LL<BinTrie<INTKEY,INTVAL,ORDER>, INTKEY>;
 
     constexpr static int order = ORDER;
     BinTrie() { }
-    BinTrie(int unused) {
+    BinTrie(INTKEY aKey) : mKey(aKey) {
     }
 
     BinTreeLink*make_key(INTKEY cidx) {
@@ -31,15 +33,15 @@ struct BinTrie
             cur->children =  make_unique<BinTreeLink>(cidx);
             assert(cur->children);
             return cur->children.get();
-        } else if (sibling->key == cidx) {
+        } else if (sibling->node.mKey == cidx) {
             return sibling;
-        } else if (sibling->key < cidx) {
+        } else if (sibling->node.mKey < cidx) {
             // insert before sibling
-            while(sibling->key < cidx and sibling->next and sibling->next->key <= cidx) {
+            while(sibling->node.mKey < cidx and sibling->next and sibling->next->node.mKey <= cidx) {
                 sibling = sibling->next.get();
             }
-            assert(sibling->key <= cidx);
-            if(sibling->key != cidx) {
+            assert(sibling->node.mKey <= cidx);
+            if(sibling->node.mKey != cidx) {
                 auto sibling2 = std::move(sibling->next);
                 sibling->next = make_unique<BinTreeLink>(cidx);
                 sibling->next->next = std::move(sibling2);
@@ -47,7 +49,7 @@ struct BinTrie
                 assert(sibling);
             }
             return sibling;
-        } else if(sibling->key > cidx) {
+        } else if(sibling->node.mKey > cidx) {
             // insert at first position
             auto sibling2 = std::move(cur->children);
             cur->children =  make_unique<BinTreeLink>(cidx);
@@ -66,27 +68,21 @@ struct BinTrie
         }
         auto x = given;
         for(int depth = 0;; depth++) {
-            for(int i = (ORDER-1); i >= 0 ;i--) {
-                INTKEY cidx = x & (i<<depth);
-                cidx >>= depth;
-                if(i == cidx) {
-                    auto*child = cur->make_key(cidx);
-                    assert(child);
-                    cur = &child->node;
-                    x -= (i<<depth);
-                    if(not x) {
-                        cur->content = value;
-                        return;
-                    }
-                    break;
-                }
+            INTKEY cidx = (x>>depth) & (ORDER-1);
+            auto*child = cur->make_key(cidx);
+            assert(child);
+            cur = &child->node;
+            x -= (cidx<<depth);
+            if(not x) {
+                cur->content = value;
+                return;
             }
         }
     }
 
     unique_ptr<BinTreeLink> children;
     optional<INTVAL> content;
-    //size_t numNodes = 0;
+    INTKEY mKey = 0;
 };
 
 
@@ -122,9 +118,9 @@ public:
             auto*sibleft = left->children.get();
             auto*sibright = right->children.get();
             while(sibleft and sibright) {
-                if(sibleft->key < sibright->key) {
+                if(sibleft->node.mKey < sibright->node.mKey) {
                     sibleft = sibleft->next.get();
-                } else if(sibleft->key > sibright->key) {
+                } else if(sibleft->node.mKey > sibright->node.mKey) {
                     sibright = sibright->next.get();
                 } else {
                     // when equal
@@ -137,3 +133,4 @@ public:
         return ret;
     }
 };
+
